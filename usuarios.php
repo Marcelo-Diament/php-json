7696c3763c2ed8babe71a0d9a037e941de8d57c7
Marcelo Diament <marcelodiament@gmail.com>
2020-03-12 19:33:42 -0300

    Listando usuários cadastrados no JSON
    A função file_get_contents() recebe o caminho (endereço) do arquivo JSON como parâmetro e captura o conteúdo do JSON (que é, no final das contas, apenas uma string).
    
    E com a função json_decode(), nós transformamos essa mesma string em um objeto.
    
    Por fim, nesse caso, utilizamos um loop foreach para percorrermos os objetos do nosso JSON e incluirmos cada usuário em uma nova linha de nossa tabela.
    
    Perceba que para acessarmos propriedades (ou métodos) de um objeto, usamos a single arrow, a famosa flechinha ou setinha: ->.

--- usuarios.php
+++ usuarios.php
@@ -1,6 +1,27 @@
 <?php $tituloPagina = "Lista de Usuários"; ?>
 <?php require_once("./inc/head.php"); ?>
 <?php require_once("./inc/header.php"); ?>
+
+<!-- CAPTURANDO DADOS DO JSON -->
+<?php
+    
+    $usuariosJson = file_get_contents("./data/usuarios.json");
+    // DESCOMENTE AS 3 LINHAS ABAIXO PARA VER O JSON DA MANEIRA QUE RECEBEMOS
+    // echo "<br><pre>";
+    // var_dump($usuariosJson);
+    // echo "</pre><br>";
+    // Perecba que o JSON nada mais é do que uma String. =)
+
+    $usuariosObjetos = json_decode($usuariosJson);
+    // DESCOMENTE AS 3 LINHAS ABAIXO PARA VER O JSON DECODIFICADO
+    // echo "<br><pre>";
+    // var_dump($usuariosObjetos);
+    // echo "</pre><br>";
+    // Ao decodificarmos o JSON, transformamos aquela string em um objeto populado por arrays e objetos!
+
+?>
+<!-- /CAPTURANDO DADOS DO JSON -->
+
     <main class="container">
         <article class="row">
             <section class="col-12 mx-auto bg-light my-5 py-5 rounded border" id="usuariosTb">
@@ -15,18 +36,52 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                        <th scope="row">Fulano</th>
-                        <td>da Silva</td>
-                        <td>email@email.com</td>
-                        <td>123456</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">Ciclano</th>
-                        <td>Santos</td>
-                        <td>ciclano@santos.com</td>
-                        <td>124155</td>
-                        </tr>
+
+                        <!-- HTML 'CHAPADO' NO CÓDIGO -->
+                        <!-- Mas queremos puxar os usuários do JSON,
+                        para não precisarmos criar cada usuário na mão -->
+
+                            <!--
+                            <tr>
+                                <th scope="row">Fulano</th>
+                                <td>da Silva</td>
+                                <td>email@email.com</td>
+                                <td>123456</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Ciclano</th>
+                                <td>Santos</td>
+                                <td>ciclano@santos.com</td>
+                                <td>124155</td>
+                            </tr>
+                            -->
+
+                        <!-- /HTML 'CHAPADO' NO CÓDIGO -->
+
+                        <!-- CAPTURANDO REGISTROS DO JSON -->
+                        
+                        <!-- 1) Nós já 'puxamos' o arquivo do Json com o file_get_contents().-->
+
+                        <!-- 2) Depois decodificamos a string que recebemos (o JSON em si) e
+                        transformamos essa string em um objeto - com o json_decode. -->
+
+                        <!-- 3) E agora vamos percorrer esses objetos com um loop foreach: -->
+
+                        <?php foreach($usuariosObjetos->usuarios as $usuario): ?>
+                            
+                            <tr>
+
+                                <th scope="row"><?= $usuario->nome; ?></th>
+                                <td><?= $usuario->sobrenome; ?></td>
+                                <td><?= $usuario->email; ?></td>
+                                <td><?= $usuario->senha; ?></td>
+
+                            </tr>
+    
+                        <?php endforeach; ?>
+
+                        <!-- /CAPTURANDO REGISTROS DO JSON -->
+
                     </tbody>
                 </table>
             </section>
